b38ad84369489510878594aca7c2c7d8bf4c6100
Julie  McManus <julieannesays@gmail.com>
2020-01-22 17:48:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,11 +1,11 @@
 def create_an_empty_array
-  Array.new
+  []
 end
 
 def create_an_array
-  Array.new(4)
+  [" ", " ", " ", " "]
 end
 
-def add_element_to_end_of_array(array)
+def add_element_to_end_of_array(array, index)
   create_an_array << array
 end
